ad6bea53925b913e62d524e25d76ba5a3522d354
ksmit323 <ksmit323@gmail.com>
2025-02-05 18:54:31 +0700

    add mobile viewport

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use dioxus::prelude::*;
 
 mod components;
-mod views;
 mod routes;
+mod views;
 use routes::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
@@ -16,6 +16,11 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Meta { 
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        }
+        
         // Global app resources
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
