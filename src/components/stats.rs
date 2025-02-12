14b5f63ef4c61e1aabad7651a9108cd2a4bc1db6
ksmit323 <ksmit323@gmail.com>
2025-02-12 23:13:58 +0700

    add stats styling

--- src/components/stats.rs
+++ src/components/stats.rs
@@ -1,29 +1,37 @@
 use dioxus::prelude::*;
+use crate::components::drinks::DRINK_TYPES;
+
+const STATS_CSS: Asset = asset!("assets/styling/stats.css");
 
 #[component]
 pub fn Stats() -> Element {
     rsx! {
-        div {
-            // Div for the Drink counts
-            div {
-                // map out each drink for their own div
-                div {
-                    // div for each drink name
-                    div {
-                        span { "Drink name" }
-                    }
-                    span { "Drink Count" }
+        document::Link { rel: "stylesheet", href: STATS_CSS }
+        
+        div { class: "stats-container",
+            div { class: "drink-counts-container",
+                {
+                    DRINK_TYPES.iter().map(|drink|{
+                        rsx! {
+                            div { class: "drink-count", key: "{drink.label}",
+                                div { class: "drink-type",
+                                    span { class: "drink-count-icon", "{drink.emoji}" }
+                                    span { class: "drink-count-label","{drink.label}" }
+                                }
+                                span { class: "drink-count-value", "Drink Count" }
+                            }
+                        }
+                    })
                 }
-            }
-            // Div for the total count and BAC
-            div {
-                div {
-                    span { "Total Drinks: " }
-                                // span { "{total_drinks}"}
+            },
+            div { class: "drink-stats",
+                div { class: "total-drinks",
+                    span { "Total Drinks: " },
+                    span { "10"}
                 }
-                div {
-                    span { "Estimated BAC: " }
-                                // span { "{bac}" }
+                div { class: "bac",
+                    span { "Estimated BAC: " },
+                    span { "0.08" }
                 }
             }
         }
