406e2b1620611203088388112f216848026af4d4
ksmit323 <ksmit323@gmail.com>
2025-02-12 19:41:19 +0700

    update end night button

--- src/components/end_night_button.rs
+++ src/components/end_night_button.rs
@@ -1,35 +1,35 @@
 use dioxus::prelude::*;
 use dioxus_motion::prelude::*;
 use easer::functions::Easing;
+use std::time::Duration;
 
 const END_NIGHT_BUTTON_CSS: Asset = asset!("/assets/styling/end_night_button.css");
 
 #[component]
 pub fn EndNightButton() -> Element {
     let mut progress = use_motion(0.0f32);
-    let is_dragging = use_signal(|| false);
-    let start_x = use_signal(|| 0.0f32);
-    let slider_width = use_signal(|| 0.0f32);
+    let mut is_holding = use_signal(|| false);
+    let animation_duration = Duration::from_secs(3);
+    
+    // Convert progress to CSS custom property
+    let progress_style = format!("--progress: {};", progress.get_value());
+        
+    let mut handle_start_progress = move || {
+        is_holding.set(true);
+        progress.animate_to(
+            100.0,
+            AnimationConfig::new(AnimationMode::Tween(Tween {
+                duration: animation_duration,
+                easing: easer::functions::Sine::ease_in_out,
+            })),
+        );
+    };    
 
-    let handle_start = move |event: MouseEvent| {
-        is_dragging.set(true);
-        start_x.set(event.client_x() as f32);
-    };
-
-    let handle_move = move |event: MouseEvent| {
-        if !is_dragging() {
-            return;
-        }
-
-        let delta = event.client_x() as f32 - start_x();
-        let new_progress = (delta / slider_width()).clamp(0.0, 1.0) * 100.0;
-        progress.set_value(new_progress);
-    };
-
-    let handle_end = move |_| {
-        if progress.get_value() >= 100.0 {
-            // Trigger completion action
-        } else {
+    let mut handle_stop_progress = move || {
+        is_holding.set(false);
+        progress.stop();
+        
+        if progress.get_value() < 100.0 {
             progress.animate_to(
                 0.0,
                 AnimationConfig::new(AnimationMode::Tween(Tween {
@@ -38,46 +38,29 @@ pub fn EndNightButton() -> Element {
                 })),
             );
         }
-        is_dragging.set(false);
     };
+    
+    let start_progress_mouse = move |_: Event<MouseData>| handle_start_progress();
+    let stop_progress_mouse = move |_: Event<MouseData>| handle_stop_progress();
+    
+    let start_progress_touch = move |_: Event<TouchData>| handle_start_progress();
+    let stop_progress_touch = move |_: Event<TouchData>| handle_stop_progress();
 
     rsx! {
         document::Link { rel: "stylesheet", href: END_NIGHT_BUTTON_CSS }
         
         div { class: "end-night-container",
-            div { class: "end-night-card",
-                div { class: "counter-text", "{progress.get_value().round() as i32}%" }
-
-                div {
-                    class: "slider-container",
-                    onmounted: move |cx| {
-                        let rect = cx.get_client_rect();
-                        slider_width.set(rect.width() as f32);
-                    },
-                    
-                    div { class: "slider-track" }
-                    div {
-                        class: "slider-progress",
-                        style: "--progress-width: {progress.get_value()}%"
-                    }
-                    div {
-                        class: "slider-thumb",
-                        style: "left: {progress.get_value()}%",
-                        onmousedown: handle_start,
-                        ontouchstart: |e| {
-                            is_dragging.set(true);
-                            start_x.set(e.touches().get(0).client_x() as f32);
-                        },
-                        ontouchmove: |e| {
-                            if is_dragging() {
-                                let delta = e.touches().get(0).client_x() as f32 - start_x();
-                                let new_progress = (delta / slider_width()).clamp(0.0, 1.0) * 100.0;
-                                progress.set_value(new_progress);
-                            }
-                        },
-                        ontouchend: handle_end,
-                        onmouseup: handle_end,
-                    }
+            button { 
+                class: "end-night-button",
+                style: "{progress_style}",
+                onmousedown: start_progress_mouse,
+                onmouseup: stop_progress_mouse,
+                onmouseleave: stop_progress_mouse,
+                ontouchstart: start_progress_touch,
+                ontouchend: stop_progress_touch,
+                
+                if !*is_holding.read() {
+                    "Hold to call it a night!"
                 }
             }
         }
