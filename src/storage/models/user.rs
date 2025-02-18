9caf9606eaa0f934ca2d2206db56abba64f94789
ksmit323 <ksmit323@gmail.com>
2025-02-18 15:23:40 +0700

    add default trait

--- src/storage/models/user.rs
+++ src/storage/models/user.rs
@@ -13,6 +13,15 @@ pub struct User {
     pub gender: Gender,
 }
 
+impl Default for User {
+    fn default() -> Self {
+        User {
+            weight: 150.0,
+            gender: Gender::Other,
+        }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct AppSettings {
     pub user: Option<User>,
