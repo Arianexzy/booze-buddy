a55381e635f60640fd2da8cc28aca65d222f9044
ksmit323 <ksmit323@gmail.com>
2025-02-19 21:28:28 +0700

    add end session mesages

--- src/components/utils/witty_messages.rs
+++ src/components/utils/witty_messages.rs
@@ -1,18 +1,19 @@
 use rand::seq::IndexedRandom;
 
 pub struct WittyMessageBank {
-    messages: Vec<&'static str>,
+    active_session_messages: Vec<&'static str>,
+    end_session_messages: Vec<&'static str>,
 }
 
 impl WittyMessageBank {
     pub fn new() -> Self {
         Self {
-            messages: vec![
+            active_session_messages: vec![
                 "Another one bites the dust! 🍺",
                 "Drunk you is tomorrow's problem",
                 "You're not drunk, you're just speaking cursive",
                 "Your liver just filed for divorce! 💔",
-                "Text your ex! JK DON'T DO IT! 📱",
+                "Text your ex!",
                 "Water sucks, it really really sucks",
                 "Future hangover loading... 🔄",
                 "Your Uber driver's gonna love this! 🚗",
@@ -23,13 +24,35 @@ impl WittyMessageBank {
                 "Your dignity left the chat! 👋",
                 "Making memories you won't remember! 📸",
                 "Bad decision time 😎",
+                "What seems to be the officer, problem?",
+                "I thought this was America",
+                "Kick some ass, sea bass",
+            ],
+            end_session_messages: vec![
+                "Nighty night sweet tits",
+                "Lay your weary head, you gorgeous legend",
+                "Sweet dreams, you boozed up beast",
+                "Goodnight, you glorious drunk",
+                "See ya, you magnificent piss-artist",
+                "Off to bed now, you sexy lush",
+                "Lights out, you booze-hound",
+                "Sweet dreams, you magnificent bastard",
+                "Sleep well, you beautiful, drunken legend",
             ],
         }
     }
 
-    pub fn get_random_message() -> String {
+    pub fn get_random_active_session_message() -> String {
         let bank = Self::new();
-        bank.messages
+        bank.active_session_messages
+            .choose(&mut rand::rng())
+            .unwrap_or(&"¯\\_(ツ)_/¯")
+            .to_string()
+    }
+
+    pub fn get_random_end_session_message() -> String {
+        let bank = Self::new();
+        bank.end_session_messages
             .choose(&mut rand::rng())
             .unwrap_or(&"¯\\_(ツ)_/¯")
             .to_string()
