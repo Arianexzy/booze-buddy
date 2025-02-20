95bad6377da08fe47e84fc82f7c90521fa48d584
ksmit323 <ksmit323@gmail.com>
2025-02-20 17:41:26 +0700

    add witty message animation

--- src/components/witty_message.rs
+++ src/components/witty_message.rs
@@ -5,15 +5,23 @@ const WITTY_MESSAGE_CSS: Asset = asset!("assets/styling/witty_message.css");
 #[derive(PartialEq, Props, Clone)]
 pub struct WittyMessageProps {
     message: String,
+    animation_trigger: Signal<i32>,
 }
 
 #[component]
 pub fn WittyMessage(props: WittyMessageProps) -> Element {
+    // @dev I could not get the animation rerun on each update
+    // I tried everything I could.  The only thing that worked
+    // was creating new class names and animation names and rotating
+    // thru them with the modulo operator
+
+    let rotation_counter = *&*props.animation_trigger.read_unchecked();
+    let dynamic_class = format!("witty-message dynamic-{}", rotation_counter % 3);
+
     rsx! {
         document::Link { rel: "stylesheet", href: WITTY_MESSAGE_CSS }
-
         div { class: "witty-message-container",
-            p { class: "witty-message", "{props.message}" }
+            p { class: "{dynamic_class}", "{props.message}" }
         }
     }
 }
--- src/views/tonight.rs
+++ src/views/tonight.rs
@@ -12,17 +12,21 @@ pub fn TonightView() -> Element {
     let mut total_drinks_resource =
         use_resource(move || async move { get_total_drinks().unwrap_or(0) });
     let mut bac_resource = use_resource(move || async move { get_current_bac().unwrap_or(0.0) });
-    let mut witty_message = use_signal(|| "Ready for poor decisions?".to_string());
     let mut reset_drink_count = use_signal(|| 0);
+
+    let mut witty_message =
+        use_signal(|| "Ready for poor decisions? Double click a drink!".to_string());
+    let mut animation_trigger = use_signal(|| 0);
 
     let update_view = move |_: ()| {
         refresh_stats(&mut total_drinks_resource, &mut bac_resource);
         witty_message.set(WittyMessageBank::get_random_active_session_message());
+        animation_trigger.set(animation_trigger() + 1);
     };
 
     let reset_view = move |_: ()| {
         refresh_stats(&mut total_drinks_resource, &mut bac_resource);
-        reset_drink_count.set(reset_drink_count() + 1);
+        reset_drink_count.set(reset_drink_count() + 1); // force update
         witty_message.set(WittyMessageBank::get_random_end_session_message());
     };
 
@@ -32,11 +36,12 @@ pub fn TonightView() -> Element {
                 DynamicBackground { total_drinks: total_drinks_resource().unwrap_or(0) }
             }
             Stats { total_drinks_resource, bac_resource }
-            WittyMessage { message: witty_message(), key: witty_message() }
-            Drinks { on_drink_added: update_view, reset_drink_count }
-            if !has_active_session() {
-                h3 { class: "view-header", "Double click a drink to start!"}
+            WittyMessage {
+                message: witty_message(),
+                animation_trigger,
+                key: witty_message(),
             }
+            Drinks { on_drink_added: update_view, reset_drink_count }
             TonightAchievements {}
             EndNightButton { on_end_night: reset_view }
         }
