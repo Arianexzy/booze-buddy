dfa50afa25adc065192ee8eed957700c1b69c9bd
ksmit323 <ksmit323@gmail.com>
2025-02-24 22:03:36 +0700

    add styling to end button slider

--- src/components/end_night_button.rs
+++ src/components/end_night_button.rs
@@ -1,67 +1,56 @@
 use crate::storage::storage::drink_history::end_current_session;
 use dioxus::prelude::*;
-use dioxus_motion::prelude::*;
-use easer::functions::Easing;
-use std::time::Duration;
 
-const END_NIGHT_BUTTON_CSS: Asset = asset!("/assets/styling/end_night_button.css");
+const END_NIGHT_SLIDER_CSS: Asset = asset!("/assets/styling/end_night_slider.css");
 
 #[derive(PartialEq, Clone, Props)]
-pub struct EndNightButtonProps {
+pub struct EndNightSliderProps {
     on_end_night: EventHandler<()>,
 }
 
 #[component]
-pub fn EndNightButton(props: EndNightButtonProps) -> Element {
-    // let mut progress = use_motion(0.0f32);
-    let mut is_holding = use_signal(|| false);
-    let animation_duration = Duration::from_millis(2500);
-
-    // Convert progress to CSS custom property
-    // let progress_style = format!("--progress: {};", progress.get_value());
-
-    // let mut handle_start_progress = move || {
-    //     is_holding.set(true);
-    //     progress.animate_to(
-    //         100.0,
-    //         AnimationConfig::new(AnimationMode::Tween(Tween {
-    //             duration: animation_duration,
-    //             easing: easer::functions::Sine::ease_in_out,
-    //         })),
-    //     );
-    // };
-
-    // let mut handle_stop_progress = move || {
-    //     is_holding.set(false);
-    //     progress.stop();
-
-    //     if progress.get_value() < 100.0 {
-    //         progress.animate_to(
-    //             0.0,
-    //             AnimationConfig::new(AnimationMode::Tween(Tween {
-    //                 duration: Duration::from_millis(500),
-    //                 easing: easer::functions::Sine::ease_out,
-    //             })),
-    //         );
-    //     }
-    // };
-
-    // let start_progress_mouse = move |_: Event<MouseData>| handle_start_progress();
-    // let stop_progress_mouse = move |_: Event<MouseData>| handle_stop_progress();
-
-    // let start_progress_touch = move |_: Event<TouchData>| handle_start_progress();
-    // let stop_progress_touch = move |_: Event<TouchData>| handle_stop_progress();
+pub fn EndNightSlider(props: EndNightSliderProps) -> Element {
+    let mut value = use_signal(|| 0.0);
+    
+    let handle_input = move |event: FormEvent | {
+        let incoming_value = event.value().parse::<f32>().unwrap_or(0.0);
+        value.set(incoming_value);
+        
+        if value() >= 0.95 {
+            props.on_end_night.call(());
+            end_current_session().expect("Failed to end current session");
+            value.set(0.0);
+        } 
+    };
+    
+    let handle_touch_end = move |_: TouchEvent| {
+        if value() < 0.95 {
+            value.set(0.0);
+        }
+    };
 
     rsx! {
-        document::Link { rel: "stylesheet", href: END_NIGHT_BUTTON_CSS }
-        div { class: "end-night-container",
-            button {
-                class: "end-night-button",
-                ondoubleclick: move |_| {
-                    props.on_end_night.call(());
-                    end_current_session().expect("Failed to end current session");
-                },
+        document::Link { rel: "stylesheet", href: END_NIGHT_SLIDER_CSS }
+        div {
+            class: "slider-container",
+            span { class: "slider-text", 
+                if value() == 0.0 {
+                    "Slide to end the night"
+                } else {
+                    "Ending night..."
+                }
+            },
+            input {
+                class: "slider-track",
+                r#type: "range",
+                value: "{value}",
+                min: "0.0",     
+                max: "1.0",
+                step: "0.01",
+                // prevent_default: "oninput", // Let's remove this for now, see if default behavior is okay
+                oninput: handle_input,
+                ontouchend: handle_touch_end,
             }
         }
     }
-}
+}   
