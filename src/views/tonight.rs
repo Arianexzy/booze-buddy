82f8875ea3613c2cc55119ae646914cfffe537a9
ksmit323 <ksmit323@gmail.com>
2025-02-24 22:08:03 +0700

    refactor

--- src/views/tonight.rs
+++ src/views/tonight.rs
@@ -1,6 +1,6 @@
 use crate::{
     components::{
-        Drinks, DynamicBackground, EndNightButton, Stats, TonightAchievements, WittyMessage,
+        Drinks, DynamicBackground, EndNightSlider, Stats, TonightAchievements, WittyMessage,
         WittyMessageBank,
     },
     storage::storage::drink_history::{get_current_bac, get_total_drinks, has_active_session},
@@ -9,30 +9,32 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn TonightView() -> Element {
+    // --- HOOKS ---
     let mut total_drinks_resource =
         use_resource(move || async move { get_total_drinks().unwrap_or(0) });
     let mut bac_resource = use_resource(move || async move { get_current_bac().unwrap_or(0.0) });
     let mut reset_drink_count = use_signal(|| 0);
-
     let mut witty_message =
         use_signal(|| "Ready for poor decisions? Double click a drink!".to_string());
     let mut animation_trigger = use_signal(|| 0);
 
+    // --- VIEW HANDLERS ---
     let update_view = move |_: ()| {
         refresh_stats(&mut total_drinks_resource, &mut bac_resource);
         witty_message.set(WittyMessageBank::get_random_active_session_message());
-        animation_trigger.set(animation_trigger() + 1);
+        trigger_witty_message_animation(&mut animation_trigger);
     };
-
     let reset_view = move |_: ()| {
         refresh_stats(&mut total_drinks_resource, &mut bac_resource);
-        reset_drink_count.set(reset_drink_count() + 1); // force update
+        trigger_drink_count_update(&mut reset_drink_count);
         witty_message.set(WittyMessageBank::get_random_end_session_message());
     };
 
+    let has_active_session = has_active_session();
+
     rsx! {
         div { class: "tonight-view-container",
-            if has_active_session() {
+            if has_active_session {
                 DynamicBackground { total_drinks: total_drinks_resource().unwrap_or(0) }
             }
             Stats { total_drinks_resource, bac_resource }
@@ -42,8 +44,10 @@ pub fn TonightView() -> Element {
                 key: witty_message(),
             }
             Drinks { on_drink_added: update_view, reset_drink_count }
-            TonightAchievements {}
-            EndNightButton { on_end_night: reset_view }
+            if has_active_session {
+                TonightAchievements {}
+                EndNightSlider { on_end_night: reset_view }
+            }
         }
     }
 }
@@ -51,4 +55,12 @@ pub fn TonightView() -> Element {
 fn refresh_stats(total_drinks_resource: &mut Resource<i32>, bac_resource: &mut Resource<f32>) {
     total_drinks_resource.restart();
     bac_resource.restart();
+}
+
+fn trigger_witty_message_animation(animation_trigger: &mut Signal<i32>) {
+    animation_trigger.set(animation_trigger() + 1);
+}
+
+fn trigger_drink_count_update(reset_drink_count: &mut Signal<i32>) {
+    reset_drink_count.set(reset_drink_count() + 1);
 }
