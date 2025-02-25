1400482efd53c236ccc06cd00054ef79972c0c4e
ksmit323 <ksmit323@gmail.com>
2025-02-25 17:42:51 +0700

    add achievements

--- src/storage/models/mod.rs
+++ src/storage/models/mod.rs
@@ -1,10 +1,12 @@
+mod achievements;
 mod constants;
 mod drink_history;
 mod drink_session;
 mod drink_type;
 mod user;
 
-// pub use constants::{DRINK_ALCOHOL_CONTENT, METABOLISM_RATE};
+pub use achievements::{Achievement, AchievementRegistry};
+pub use constants::{DRINK_ALCOHOL_CONTENT, METABOLISM_RATE};
 pub use drink_history::DrinkingHistory;
 pub use drink_session::DrinkingSession;
 pub use drink_type::{DrinkEvent, DrinkType};
