fa555e18774acf660361f1f96a22a4876782c31f
ksmit323 <ksmit323@gmail.com>
2025-02-26 12:08:05 +0700

    add pulse

--- src/components/witty_message.rs
+++ src/components/witty_message.rs
@@ -1,5 +1,7 @@
 use dioxus::prelude::*;
 
+use crate::storage::storage::drink_history::has_active_session;
+
 const WITTY_MESSAGE_CSS: Asset = asset!("assets/styling/witty_message.css");
 
 #[derive(PartialEq, Props, Clone)]
@@ -17,10 +19,15 @@ pub fn WittyMessage(props: WittyMessageProps) -> Element {
 
     let rotation_counter = *&*props.animation_trigger.read_unchecked();
     let dynamic_class = format!("witty-message dynamic-{}", rotation_counter % 3);
+    
+    let container_class = match has_active_session() {
+        true => "witty-message-container",
+        false => "witty-message-container pulse",
+    };
 
     rsx! {
         document::Link { rel: "stylesheet", href: WITTY_MESSAGE_CSS }
-        div { class: "witty-message-container",
+        div { class: "{container_class}",
             p { class: "{dynamic_class}", "{props.message}" }
         }
     }
