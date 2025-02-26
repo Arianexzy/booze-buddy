a74036e47db398bd1b304f511e7078da51a1b9cb
ksmit323 <ksmit323@gmail.com>
2025-02-27 00:37:15 +0700

    update constants

--- src/storage/models/mod.rs
+++ src/storage/models/mod.rs
@@ -7,7 +7,7 @@ mod user;
 mod witty_messages;
 
 pub use achievements::{Achievement, AchievementRegistry, AchievementTier};
-pub use constants::{DRINK_ALCOHOL_CONTENT, METABOLISM_RATE};
+pub use constants::{ALCOHOL_CONVERSION_FACTOR, DRINK_ALCOHOL_CONTENT, METABOLISM_RATE};
 pub use drink_history::DrinkingHistory;
 pub use drink_session::DrinkingSession;
 pub use drink_type::*;
